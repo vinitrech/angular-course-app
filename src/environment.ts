ab8d4fbcbe851ec7536771d91c7e43d2c663ad56
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2023-02-17 20:55:49 -0300

    subindo environment.ts

---
+++ src/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://angular-recipes-8a6cc-default-rtdb.firebaseio.com/recipes.json',
+    apiKey: 'AIzaSyBh9Z_5iJSnVL1oPiNewa814QfZ35Wg8tg',
+    apiSignUpUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp',
+    apiSignInUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword'
+};
