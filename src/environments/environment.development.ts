9d2b399a77a70017721b4d9d545c17c5f7c95758
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2023-02-17 21:23:01 -0300

    subindo tudo

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://angular-recipes-8a6cc-default-rtdb.firebaseio.com/recipes.json',
+    apiKey: 'AIzaSyBh9Z_5iJSnVL1oPiNewa814QfZ35Wg8tg',
+    apiSignUpUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp',
+    apiSignInUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword'
+};
+
+// 'ng build --configuration development' uses this environment, as configured in angular.json
